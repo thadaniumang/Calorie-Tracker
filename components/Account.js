221f3db23ab11cf89aa7b4195c73411343b28a12
Umang Thadani <thadaniumang@outlook.com>
2022-12-05 00:59:49 +0530

    Add: Boilerplate with SupaAuth

---
+++ components/Account.js
@@ -0,0 +1,19 @@
+import { useState, useEffect } from 'react'
+import { useUser, useSupabaseClient } from '@supabase/auth-helpers-react'
+
+export default function Account({ session }) {
+  const supabase = useSupabaseClient()
+  const user = useUser()
+
+  return (
+    <div>
+      <h2>This is gonna be a Dashboard</h2>
+
+      <div>
+        <button className="button block" onClick={() => supabase.auth.signOut()}>
+          Sign Out
+        </button>
+      </div>
+    </div>
+  )
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
 import '../styles/globals.css'
+import { useState } from 'react'
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
+import { SessionContextProvider } from '@supabase/auth-helpers-react'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+  const supabase_project_url = process.env.NEXT_PUBLIC_SUPABASE_URL
+  const supabase_project_anon_key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  const [supabase] = useState(() => createBrowserSupabaseClient(supabase_project_url, supabase_project_anon_key))
 
+  return (
+    <SessionContextProvider supabaseClient={supabase} initialSession={pageProps.initialSession}>
+      <div className='sm:w-2/3 md:w-3/5 lg:w-1/2 mx-auto px-8 py-16'>
+        <Component {...pageProps} />
+      </div>
+    </SessionContextProvider>
+  )
+}
 export default MyApp
